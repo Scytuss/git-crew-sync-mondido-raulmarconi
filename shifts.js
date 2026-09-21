f104c31d907aaffe1c30a215225e85a49fd0a528
Scytuss <raulmondido13@gmail.com>
2026-09-21 22:38:48 +0800

    Add double-time pay for 12 hour shifts or more - mondido.raulmarconi

--- shifts.js
+++ shifts.js
@@ -3,7 +3,18 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+  if (hours <= 8) {
+    return Math.floor(hours * rate);
+  }
+  if (hours <= 12) {
+    const regularPay = 8 * rate;
+    const overtimePay = (hours - 8) * rate * 1.5;
+    return Math.floor(regularPay + overtimePay);
+  }
+  const regularPay = 8 * rate;
+  const overtimePay = 4 * rate * 1.5;
+  const doubleTimePay = (hours - 12) * rate * 2;
+  return Math.floor(regularPay + overtimePay + doubleTimePay);
 }
 
 module.exports = { isValidShift, calculatePay };
